f3c841fb5e0ed2ae5bbea1a8ef88c1e8ff5f2b50
jlw4049 <jlw_4049@hotmail.com>
2020-10-20 13:04:21 -0400

    - Checks for exsist folders, if not present it creates them

---
+++ Packages/DirectoryCheck.py
@@ -0,0 +1,33 @@
+import pathlib
+import shutil
+
+# Checks for App Folder and Sub-Directories - Creates Folders if they are missing -------------------------------------
+
+def directory_check():
+    if shutil.which('ffmpeg') != None:
+        pass
+    elif shutil.which('ffmpeg') == None:
+        ffmpeg_folder = pathlib.Path.cwd() / 'Apps' / 'FFMPEG'
+    mediainfo_folder = pathlib.Path.cwd() / 'Apps' / 'MediaInfo'
+    mediainfocli_folder = pathlib.Path.cwd() / 'Apps' / 'MediaInfoCLI'
+    fdkaac_folder = pathlib.Path.cwd() / 'Apps' / 'fdkaac'
+    qaac_folder = pathlib.Path.cwd() / 'Apps' / 'qaac'
+    mpv_player_folder = pathlib.Path.cwd() / 'Apps' / 'mpv'
+    youtube_dl_folder = pathlib.Path.cwd() / 'Apps' / 'youtube-dl'
+
+    try:
+        if shutil.which('ffmpeg') != None:
+            pass
+        elif shutil.which('ffmpeg') == None:
+            ffmpeg_folder.mkdir(parents=True, exist_ok=False)
+        youtube_dl_folder.mkdir(parents=True, exist_ok=False)
+        mediainfo_folder.mkdir(parents=True, exist_ok=False)
+        mediainfocli_folder.mkdir(parents=True, exist_ok=False)
+        fdkaac_folder.mkdir(parents=True, exist_ok=False)
+        qaac_folder.mkdir(parents=True, exist_ok=False)
+        mpv_player_folder.mkdir(parents=True, exist_ok=False)
+        fdkaac_folder.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        pass
+
+# -------------------------------------------------------------------------------------------------------- Folder Check
