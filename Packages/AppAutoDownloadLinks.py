020cee22579c60ef527466a2bd6b97e2101cc1a5
jlw4049 <jlw_4049@hotmail.com>
2020-10-20 14:23:16 -0400

    -

--- Packages/AppAutoDownloadLinks.py
+++ Packages/AppAutoDownloadLinks.py
@@ -8,6 +8,7 @@ fdkaac_url = 'http://www.mediafire.com/file/9teytvbxvd4p147/fdkaac.zip/file'
 qaac_url = 'http://www.mediafire.com/file/r432455i70yi0px/qaac.zip/file'
 mpv_player_url = 'http://www.mediafire.com/file/w291k8ewomk3hym/mpv.zip/file'
 youtubedl_url = 'https://yt-dl.org/downloads/2020.09.20/youtube-dl.exe'
+msvcr100dll_url = 'http://www.mediafire.com/file/3c0auuqeliptbw6/msvcr100.dll/file'
 
 
 # Back up links for auto downloader ---
@@ -17,8 +18,8 @@ mediainfocli_url2 = 'http://www.mediafire.com/file/0u13w72bk937xcr/MediaInfoCLI.
 fdkaac_url2 = 'http://www.mediafire.com/file/aoqu4tyyj9zq8xs/fdkaac.zip/file'
 qaac_url2 = 'http://www.mediafire.com/file/hmlk1bzlitoh2ft/qaac.zip/file'
 mpv_player_url2 = 'http://www.mediafire.com/file/1v3uqftli3ft3o0/mpv.zip/file'
-youtubedl_url2 = 'http://www.mediafire.com/file/kaer2fbfl6vaos6/youtube-dl.zip/file'
-
+youtubedl_url2 = 'http://www.mediafire.com/file/vujt353tx8lr1j4/youtube-dl.zip/file'
+msvcr100dll_url2 = 'http://www.mediafire.com/file/jyvsbkguvrrn8hj/msvcr100.dll/file'
 
 
 
