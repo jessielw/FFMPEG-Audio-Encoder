93653870ccb87a9a0d2e541d92eead623c5c0f69
jlw4049 <jlw_4049@hotmail.com>
2021-03-06 01:30:24 -0500

    v3.34 - Updates latest version of Youtube-DL-GUI

--- FFMPEGAudioEncoder.py
+++ FFMPEGAudioEncoder.py
@@ -38,7 +38,7 @@ def root_exit_function():
             root.destroy()
 
 root = TkinterDnD.Tk()
-root.title("FFMPEG Audio Encoder v3.33")
+root.title("FFMPEG Audio Encoder v3.34")
 root.iconphoto(True, PhotoImage(file="Runtime/Images/topbar.png"))
 root.configure(background="#434547")
 window_height = 220
