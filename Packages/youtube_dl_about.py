6c41b25487435f3e01bd1f18145ee92f5cc83170
jlw4049 <jlw_4049@hotmail.com>
2021-04-04 22:11:35 -0400

    Update youtube_dl_about.py

--- Packages/youtube_dl_about.py
+++ Packages/youtube_dl_about.py
@@ -15,7 +15,7 @@ def openaboutwindow():
     about_window_text = Text(about_window, background="#434547", foreground="white", relief=SUNKEN)
     about_window_text.pack()
     about_window_text.configure(state=NORMAL)
-    about_window_text.insert(INSERT, "Youtube-DL-Gui v1.1 \n")
+    about_window_text.insert(INSERT, "Youtube-DL-Gui v1.35.3 \n")
     about_window_text.insert(INSERT, "\n")
     about_window_text.insert(INSERT, "Development: jlw4049")
     about_window_text.insert(INSERT, "\n\n")
