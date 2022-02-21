df7012f71faa99717cd2b8940044410f5bcf177c
jlw4049 <jlw_4049@hotmail.com>
2022-02-20 22:13:44 -0500

    Change geometry from format to f string

--- FFMPEGAudioEncoder.py
+++ FFMPEGAudioEncoder.py
@@ -42,7 +42,8 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x_coordinate = int((screen_width / 2) - (window_width / 2))
 y_coordinate = int((screen_height / 2) - (window_height / 2))
-root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_coordinate, y_coordinate))
+root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+# root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_coordinate, y_coordinate))
 root.protocol('WM_DELETE_WINDOW', root_exit_function)
 
 # Block of code to fix DPI awareness issues on Windows 7 or higher
