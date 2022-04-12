0668ef7ef9089cba8c18f2a944c6db68675231f9
jlw4049 <jlw_4049@hotmail.com>
2022-04-12 11:12:56 -0400

    Add support to save location/size of window upon close
    * Added config parser to write information needed to config.ini file

--- Packages/About.py
+++ Packages/About.py
@@ -1,9 +1,15 @@
 from tkinter import Toplevel, INSERT, Text, NORMAL, DISABLED, messagebox, SUNKEN
+from configparser import ConfigParser
 
 
 # About Window --------------------------------------------------------------------------------------------------------
 def openaboutwindow():
     global about_window
+
+    # Defines the path to config.ini and opens it for reading/writing
+    config_file = 'Runtime/config.ini'  # Creates (if doesn't exist) and defines location of config.ini
+    config = ConfigParser()
+    config.read(config_file)
 
     try:  # If "About" window is already opened, display a message, then close the "About" window
         if about_window.winfo_exists():
@@ -13,6 +19,17 @@ def openaboutwindow():
             return
     except NameError:
         pass
+
+    def about_exit_function():  # Exit function when hitting the 'X' button
+        if config['save_window_locations']['about'] == 'yes':  # If auto save position on close is checked
+            try:
+                config.set('save_window_locations', 'about position', about_window.geometry())
+                with open(config_file, 'w') as configfile:
+                    config.write(configfile)
+            except (Exception,):
+                pass
+
+        about_window.destroy()  # Close window
 
     about_window = Toplevel()
     about_window.title('About')
@@ -24,6 +41,7 @@ def openaboutwindow():
     x_cordinate = int((screen_width / 2) - (window_width / 2))
     y_cordinate = int((screen_height / 2) - (window_height / 2))
     about_window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+    about_window.protocol('WM_DELETE_WINDOW', about_exit_function)
     about_window_text = Text(about_window, background="#434547", foreground="white", relief=SUNKEN)
     about_window_text.pack()
     about_window_text.configure(state=NORMAL)
